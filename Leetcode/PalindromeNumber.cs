b97f0e898740ec4783f0347529a158ed1c02a905
Quang Phan <oclockvn@gmail.com>
2021-08-02 18:27:12 +0700

    add palindrome number

---
+++ Leetcode/PalindromeNumber.cs
@@ -0,0 +1,24 @@
+﻿namespace Leetcode
+{
+    public class PalindromeNumber
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+
+            var t = x;
+            var i = 0;
+
+            while (x > 0)
+            {
+                i = i * 10 + x % 10;
+                x /= 10;
+            }
+
+            return i == t;
+        }
+    }
+}
