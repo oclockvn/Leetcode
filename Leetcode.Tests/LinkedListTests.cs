b8d3e7f7a82f372ef2e88b91b3a0b6597af0b4e8
Quang Phan <oclockvn@gmail.com>
2021-08-06 19:58:24 +0700

    WIP

---
+++ Leetcode.Tests/LinkedListTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Leetcode.Tests
+{
+    public class LinkedListTests
+    {
+        [Theory]
+        [InlineData(new[] { 2, 4, 3 }, new[] { 5, 6, 4 }, new[] { 7, 0, 8 })]
+        [InlineData(new[] { 2, 4 }, new[] { 5, 6 }, new[] { 7, 0, 1 })]
+        [InlineData(new[] { 0 }, new[] { 0 }, new[] { 0 })]
+        [InlineData(new[] { 9, 9, 9, 9, 9, 9, 9 }, new[] { 9, 9, 9, 9 }, new[] { 8, 9, 9, 9, 0, 0, 0, 1 })]
+        [InlineData(new[] { 2, 4, 9 }, new[] { 5, 6, 4, 9 }, new[] { 7, 0, 4, 0, 1 })]
+        public void Sum_LinkedList_ShouldBeOk(int[] l1, int[] l2, int[] expected)
+        {
+            var list1 = new ListNode(l1);
+            var list2 = new ListNode(l2);
+
+            var result = new LinkedList().AddTwoNumbers(list1, list2);
+
+            Assert.Equal(expected, result.ToArray());
+        }
+    }
+}
---
+++ Leetcode/LinkedList.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Leetcode
+{
+    [DebuggerDisplay("val={val}, next={next}")]
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+
+        public ListNode(int[] arr)
+        {
+            if (arr?.Length > 0)
+            {
+                val = arr[0];
+                var nextArr = arr.ToList().Skip(1).ToArray();
+                next = nextArr.Length == 0 ? null : new ListNode(nextArr);
+            }
+        }
+
+        public int[] ToArray()
+        {
+            var arr = new List<int> { val };
+            var nextNode = next;
+            while (nextNode != null)
+            {
+                arr.Add(nextNode.val);
+                nextNode = nextNode.next;
+            }
+
+            return arr.ToArray();
+        }
+    }
+
+    public class LinkedList
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode next = l1;
+            ListNode prev = null;
+            int saved = 0;
+
+            do
+            {
+                next.val = saved + next.val + (l2?.val ?? 0);
+                saved = 0;
+
+                if (next.val > 9)
+                {
+                    saved = next.val / 10;
+                    next.val = next.val % 10;
+                }
+
+                if (l2 != null)
+                    l2 = l2.next;
+
+                prev = next;
+                next = next.next;
+                if (next == null)
+                {
+                    prev = l2;
+                    next = prev;
+
+                    //break;
+                    //if (next == null)
+                    //    break;
+                    //if (l2 != null)
+                    //{
+                    //    prev = l2;
+                    //    //l2.val += saved;
+                    //    //if (l2.val > 9)
+                    //    //{
+                    //    //    saved = l2.val / 10;
+                    //    //    l2.val = l2.val % 10;
+                    //    //}
+
+                    //    //next = l2;
+                    //    next = prev;
+                    //    //prev = next;
+                    //    //break;
+                    //}
+                }
+                //else
+                //{
+                //    next = next.next;
+                //}
+
+                if (l2 == null)
+                {
+                    //break;
+                }
+
+                //if (next == null && l2 != null)
+                //{
+                //    next = l2;
+                //    break; // cont to l2
+                //}
+
+            } while (next != null);
+
+            if (saved > 0)
+            {
+                ListNode last = l1;
+                while (last.next != null)
+                {
+                    last = last.next;
+                }
+                last.next = new ListNode(saved);
+            }
+
+            return l1;
+        }
+    }
+}
